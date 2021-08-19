45a029e33488ca8d9b21a3b12cdb41c92af53943
Alexandre Cardoso <developer.ios.alexandre@gmail.com>
2021-08-18 22:50:20 -0300

    Refatorado a class MovieService, criado um protocol para o network service e implementado a injeção de dependência na ViewModel.

--- Movies/Feature/Controller/ViewModel/MoviesViewModel.swift
+++ Movies/Feature/Controller/ViewModel/MoviesViewModel.swift
@@ -6,26 +6,35 @@
 import Foundation
 
 class MoviesViewModel {
-    private let service = MovieService()
+	
+	private let service: MovieServiceRepository
+	
+	private var completion: (([Movie]?, Error?) -> Void)?
+	
+	init(service: MovieServiceRepository = MovieService()) {
+		self.service = service
+	}
+	
+	func loadContacts(_ completion: @escaping ([Movie]?, MovieError?) -> Void) {
+		
+		service.fetchMovies { result in
+			switch result {
+				case .success(let movies):
+					completion(movies, nil)
+				case .failure(let error):
+					completion(nil, error)
+			}
+		}
 
-    private var completion: (([Movie]?, Error?) -> Void)?
-
-    init() { }
-
-    func loadContacts(_ completion: @escaping ([Movie]?, Error?) -> Void) {
-        self.completion = completion
-        service.fetchMovies { contacts, err in
-            self.handle(contacts, err)
-        }
-    }
-
-    private func handle(_ contacts: [Movie]?, _ error: Error?) {
-        if let e = error {
-            completion?(nil, e)
-        }
-
-        if let contacts = contacts {
-            completion?(contacts, nil)
-        }
-    }
+	}
+	
+	private func handle(_ contacts: [Movie]?, _ error: Error?) {
+		if let e = error {
+			completion?(nil, e)
+		}
+		
+		if let contacts = contacts {
+			completion?(contacts, nil)
+		}
+	}
 }
