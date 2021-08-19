ab40c221e19b7a7a85014459bf062b10c12493ad
Alexandre Cardoso <developer.ios.alexandre@gmail.com>
2021-08-18 22:51:21 -0300

    Refatorado a ViewModel.

--- Movies/Feature/Controller/ViewModel/MoviesViewModel.swift
+++ Movies/Feature/Controller/ViewModel/MoviesViewModel.swift
@@ -9,14 +9,11 @@ class MoviesViewModel {
 	
 	private let service: MovieServiceRepository
 	
-	private var completion: (([Movie]?, Error?) -> Void)?
-	
 	init(service: MovieServiceRepository = MovieService()) {
 		self.service = service
 	}
 	
 	func loadContacts(_ completion: @escaping ([Movie]?, MovieError?) -> Void) {
-		
 		service.fetchMovies { result in
 			switch result {
 				case .success(let movies):
@@ -25,16 +22,6 @@ class MoviesViewModel {
 					completion(nil, error)
 			}
 		}
-
 	}
 	
-	private func handle(_ contacts: [Movie]?, _ error: Error?) {
-		if let e = error {
-			completion?(nil, e)
-		}
-		
-		if let contacts = contacts {
-			completion?(contacts, nil)
-		}
-	}
 }
