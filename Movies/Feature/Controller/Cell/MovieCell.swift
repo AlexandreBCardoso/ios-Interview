f5059a9cae8bef348572922367e164fc26a4b10f
Alexandre Cardoso <developer.ios.alexandre@gmail.com>
2021-08-18 22:28:30 -0300

    Refatorado a class MovieCell e implementado o protocol CodeView

--- Movies/Feature/Controller/Cell/MovieCell.swift
+++ Movies/Feature/Controller/Cell/MovieCell.swift
@@ -6,45 +6,57 @@
 import UIKit
 
 class MovieCell: UITableViewCell {
-    lazy var movieImage: UIImageView = {
-        let imgView = UIImageView()
-        imgView.translatesAutoresizingMaskIntoConstraints = false
-        imgView.contentMode = .scaleAspectFit
-        imgView.clipsToBounds = true
-        return imgView
-    }()
+	
+	lazy var movieImage: UIImageView = {
+		let imgView = UIImageView()
+		imgView.translatesAutoresizingMaskIntoConstraints = false
+		imgView.contentMode = .scaleAspectFit
+		imgView.clipsToBounds = true
+		return imgView
+	}()
+	
+	lazy var movieTitleLabel: UILabel = {
+		let label = UILabel()
+		label.font = UIFont.systemFont(ofSize: 20, weight: .semibold)
+		label.translatesAutoresizingMaskIntoConstraints = false
+		return label
+	}()
+	
+	
+	// MARK: - Initialize
+	override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+		super.init(style: style, reuseIdentifier: reuseIdentifier)
+		
+		setupUI()
+	}
+	
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+}
 
-    lazy var movieTitleLabel: UILabel = {
-        let label = UILabel()
-        label.font = UIFont.systemFont(ofSize: 20, weight: .semibold)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
 
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
+// MARK: - Extension CodeView
+extension MovieCell: CodeView {
+	
+	func setupSubviews() {
+		contentView.addSubview(movieImage)
+		contentView.addSubview(movieTitleLabel)
+	}
+	
+	func setupConstraints() {
+		NSLayoutConstraint.activate([
+			movieImage.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 15),
+			movieImage.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+			movieImage.heightAnchor.constraint(equalToConstant: 100),
+			movieImage.widthAnchor.constraint(equalToConstant: 100),
+			
+			movieTitleLabel.leadingAnchor.constraint(equalTo: movieImage.trailingAnchor, constant: 16),
+			movieTitleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -15),
+			movieTitleLabel.topAnchor.constraint(equalTo: contentView.topAnchor),
+			movieTitleLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+		])
+	}
 
-        configureViews()
-    }
-
-    required init?(coder: NSCoder) {
-        super.init(coder: coder)
-
-        configureViews()
-    }
-
-    func configureViews() {
-        contentView.addSubview(movieImage)
-        contentView.addSubview(movieTitleLabel)
-
-        movieImage.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 15).isActive = true
-        movieImage.centerYAnchor.constraint(equalTo: contentView.centerYAnchor).isActive = true
-        movieImage.heightAnchor.constraint(equalToConstant: 100).isActive = true
-        movieImage.widthAnchor.constraint(equalToConstant: 100).isActive = true
-
-        movieTitleLabel.leadingAnchor.constraint(equalTo: movieImage.trailingAnchor, constant: 16).isActive = true
-        movieTitleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -15).isActive = true
-        movieTitleLabel.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
-        movieTitleLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
-    }
 }
